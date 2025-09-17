f85b8e296e7e028cfa7d2c4e2a28be72d920327d
Heliacamara <camara.helia@escolar.ifrn.edu.br>
2025-09-17 17:41:50 -0300

    V2.3
     Um programa que leia três números inteiros
    nas variáveis a, b e c e escreva a média deles:
    (a+b+c)/3.

---
+++ primeiros exercecicios do 3B-IP/exercicioV2.3.py
@@ -0,0 +1,13 @@
+# @cikey f8a3d76c7a6f58f18a695f0148380a0a
+# @sid 20251174010003
+# @aid V2.3
+
+
+#begin_inputs
+a = int(input("Digite a sua primeira nota:"))
+b = int(input("Digite a sua segunda nota:"))
+c = int(input("Digite a sua terceira nota:"))
+
+#end_inputs
+
+print("{}".format(int(a+b+c)/3))
