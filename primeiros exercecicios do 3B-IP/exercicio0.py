179d69171efb0273581d2fc23cf5bc14bda4251b
Heliacamara <camara.helia@escolar.ifrn.edu.br>
2025-09-18 22:08:23 -0300

    Create exercicio0.py
    O primeiro exercício de programação efetuado com a operação matemática de adição , no qual o programa ler os dois números e obteve a soma deles

---
+++ primeiros exercecicios do 3B-IP/exercicio0.py
@@ -0,0 +1,14 @@
+# @cikey f8a3d76c7a6f58f18a695f0148380a0a
+# @sid 20251174010003
+# @aid 0
+
+
+#begin_inputs
+
+num1= int(input("Digite o primeiro numero:"))
+num2= int(input("Digite o segundo numero:"))
+
+#end_inputs
+soma= num1+num2
+print(soma)
+
