0147ceeb60b09be61b18dae2a0800554406e44ff
Heliacamara <camara.helia@escolar.ifrn.edu.br>
2025-09-23 14:03:30 -0300

    Create exercicioV3.1.py

---
+++ primeiros exercecicios do 3B-IP/exercicioV3.1.py
@@ -0,0 +1,16 @@
+# @cikey f8a3d76c7a6f58f18a695f0148380a0a
+# @sid 20251174010003
+# @aid V3.1
+
+
+#begin_inputs
+
+# Isso está aqui apenas para grantir que os testes automáticos funcionem
+# NÃO mexa aqui.
+
+#end_inputs
+
+for i in range(99,151):
+    print(i , "bugs no software,pegue um deles e conserte")
+
+
